206496ffdef372349987529dafd0054ff238351d
beingmayankmishra <beingmayankmishra@gmail.com>
2024-09-11 02:52:12 +0530

    final commit

--- index.js
+++ index.js
@@ -16,11 +16,93 @@ const winningPositions = [
 function initGame(){
      currentPlayer="x";
      gameGrid=["","","","","","","","",""];
+
+     boxes.forEach((box, index)=>{
+        box.innerText="";
+        boxes[index].style.pointerEvents = "all";
+
+        box.classList=`box box${index+1}`;
+
+
+     });
      newGameBtn.classList.remove("active");
      gameInfo.innerText=`Current Player-${currentPlayer}` ;
 }
 
 initGame();
+
+function swapTurn(){
+    if(currentPlayer==="x"){
+        currentPlayer="O";
+    }
+    else{
+        currentPlayer="x";
+    }
+
+    gameInfo.innerText=`Current Player - ${currentPlayer}`;
+}
+
+function checkGameOver(){
+   let answer="";
+   winningPositions.forEach((position) => {
+    //all 3 boxes should be non-empty and exactly same in value
+    if( (gameGrid[position[0]] !== "" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !== "") 
+        && (gameGrid[position[0]] === gameGrid[position[1]] ) && (gameGrid[position[1]] === gameGrid[position[2]])) {
+
+            //check if winner is X
+            if(gameGrid[position[0]] === "X") 
+                answer = "X";
+            else {
+                answer = "O";
+            } 
+                
+
+            //disable pointer events
+            boxes.forEach((box) => {
+                box.style.pointerEvents = "none";
+            })
+
+            //now we know X/O is a winner
+            boxes[position[0]].classList.add("win");
+            boxes[position[1]].classList.add("win");
+            boxes[position[2]].classList.add("win");
+        }
+});
+
+if(answer!=""){
+    gameInfo.innerText=`Winner Player-${answer}`;
+    newGameBtn.classList.add("active");
+    return;
+}
+
+   //check wheather there is tie
+
+   let fillCount=0;
+   gameGrid.forEach((box)=>{
+    if(box!="")
+        fillCount++;
+
+   });
+
+   if(fillCount===9){
+    gameInfo.innerText="Game Tied !";
+    newGameBtn.classList.add("active"); 
+
+   }
+
+};
+
+function handleClick(index) {
+    if(gameGrid[index] === "" ) {
+        boxes[index].innerText = currentPlayer;
+        gameGrid[index] = currentPlayer;
+        boxes[index].style.pointerEvents = "none";
+        //swap turn ko
+        swapTurn();
+        //check koi jeet toh nahi gya
+        checkGameOver();
+    }
+}
 
 boxes.forEach((box,index)=>{
     box.addEventListener("click", ()=>{
@@ -28,3 +110,5 @@ boxes.forEach((box,index)=>{
     })
 
 });
+
+newGameBtn.addEventListener("click",initGame);
